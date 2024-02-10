ef394467ec2636ce4168a326dfcfbc0e68fa062d
Nantha GS <111976540+NANTHAGS7@users.noreply.github.com>
2024-02-10 11:43:10 +0530

    Delete DAY 10 directory

--- DAY 10/src/main/java/com/birthdayevent/nanthags/repository/EdetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.birthdayevent.nanthags.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.birthdayevent.nanthags.modal.EventDetails;
-
-public interface EdetailsRepository extends JpaRepository<EventDetails,Long> {
-
-}
