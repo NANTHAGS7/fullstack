951960ddd936f815f883abe869c20bde0ab7be45
Nantha GS <111976540+NANTHAGS7@users.noreply.github.com>
2024-02-14 16:05:39 +0530

    Delete birthday_event_management./Frontend/src directory

--- birthday_event_management./Frontend/src/services/auth.js
+++
@@ -1,5 +0,0 @@
-import axios from './axios';
-const api_uri = "https://localhost:8081";
-
-export const signIn = (data)=>  axios.post('${api_uri}/api/v1/auth/login',data);
-export const signUp = (data)=> axios.post('${api_uri}/api/v1/auth/register',data);
--- birthday_event_management./Frontend/src/services/user.js
+++
@@ -1,5 +0,0 @@
-import instance from "./axios";
-
-const api_uri = "https://localhost:8081";
-
-export const userList = ()=> instance.get('{api_uri}/api/v1/user/userlist');
